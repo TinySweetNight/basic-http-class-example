9f4ffa0df7a4ea4295c842e9a669cb7e75de6738
Lily Reitz (RIT Student) <lwr3476@rit.edu>
2026-09-02 14:37:04 -0400

    new Page

---
+++ src/responses.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const client2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+
+const sendPage = (request, response, page) => {
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write(page);
+    response.end();
+
+};
+
+
+const getIndex = (request, response) => {
+    sendPage(request, response, index);
+    
+};
+
+const getClient2 = (request, response) => {
+    sendPage(request, response, client2);
+    
+};
+
+module.exports = {
+    getIndex,
+    getClient2
+};
